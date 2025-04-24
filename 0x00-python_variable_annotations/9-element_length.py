0568202074f05eae50775df61b2f1c6f8d7b046f
dohaehab <dohaehab@gmail.com>
2025-04-24 23:04:10 +0200

    task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -5,7 +5,7 @@ function 'element_length'
 from typing import Union, List, Tuple, Sequence
 
 
-def element_length(lst: Union[str, List[str]]) -> List[Tuple[str, int]]:
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
     """
         loops on the list to get the length
         Args:
