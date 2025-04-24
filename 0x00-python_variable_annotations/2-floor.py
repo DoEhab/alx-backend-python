fd55ea88e35eb2151de1dc5cd11f9aefcd01b07b
dohaehab <dohaehab@gmail.com>
2025-04-24 17:44:54 +0200

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This function return the floor of float
+"""
+import math
+
+
+def floor(n: float) -> float:
+    """
+    n: first param
+    """
+    return math.floor(n)
