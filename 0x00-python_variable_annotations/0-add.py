60086f3b0eb619c3fd2bb24edc6b56d637fa2932
dohaehab <dohaehab@gmail.com>
2025-04-24 15:37:57 +0200

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This type annotated function return
+the sum of 2 params
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    a: is the first param
+    b: is the second param
+    """
+    return a + b
