71feea8a87668e47841d18012b7efe9bc8d7e09b
dohaehab <dohaehab@gmail.com>
2025-04-24 16:07:24 +0200

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This function concat to string
+"""
+import string
+
+
+def concat(str1: string, str2: string) -> string:
+    """
+    str1: first param
+    str2: second param
+    """
+    return str1 + str2
