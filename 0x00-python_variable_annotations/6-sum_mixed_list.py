b69dc5087c075267323147b559143c7b95838aa4
dohaehab <dohaehab@gmail.com>
2025-04-24 19:50:03 +0200

    task 5

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This function return string rep of float
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(input_list: List[Union[float,int]]) -> float:
+    """
+    input_list: first param
+    """
+    result: float = 0
+    for i in input_list:
+        result = result + i
+    return result
