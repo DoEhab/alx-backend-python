e8e1ad3e879a2d468255934de1ec72c451e75b6d
dohaehab <dohaehab@gmail.com>
2025-04-29 00:00:51 +0300

    task 1

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -9,14 +9,14 @@ function measure_time
 """
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """
     :param n: number of tasks
     :param max_delay: max delay value
     :return: execution time
     """
     start_time = time.time()
-    await wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
     end_time = time.time()
     total_time = end_time - start_time
     return total_time / n
