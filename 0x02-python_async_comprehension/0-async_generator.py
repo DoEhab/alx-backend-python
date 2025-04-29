02426b1d385cbfed3d1d9d451db66a09072934b5
dohaehab <dohaehab@gmail.com>
2025-04-30 01:13:46 +0300

    task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import asyncio
+import random
+
+"""
+async_generator function
+"""
+
+
+async def async_generator():
+    """
+    coroutine function
+    that returns a random number async
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
