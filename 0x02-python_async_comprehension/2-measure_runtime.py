9cecf1dacde8bb2e38f9e6788e95c08a49017812
dohaehab <dohaehab@gmail.com>
2025-04-30 01:44:53 +0300

    task 6

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
+import asyncio
+import time
 
 """
 measure_runtime function
 """
-import asyncio
-import time
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
